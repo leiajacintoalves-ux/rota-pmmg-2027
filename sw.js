a47875b76981343d4df072c31baeea023e239bf4
Rota PMMG <leiajacintoalves@gmail.com>
2026-08-06 03:41:45 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@ const ASSETS = [
   "./lessons-penal.js",
   "./lessons-processo-penal.js",
   "./lessons-direitos-humanos.js",
-  "./lessons-matematica.js?v=2",
+  "./lessons-matematica.js?v=3",
   "./manifest.webmanifest",
   "./icon-192.png",
   "./icon-512.png",
